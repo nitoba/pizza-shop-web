5a4b36cddb78be82bd7a291e02ac3c50ea311dd8
Bruno Alves <nito.dev@gmail.com>
2024-01-20 10:20:49 -0300

    feat: handle cancel order

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,9 +1,12 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { formatDistanceToNow } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
-import { ArrowRight, Search, X } from 'lucide-react'
+import { ArrowRight, Loader2, Search, X } from 'lucide-react'
 import { useState } from 'react'
+import { toast } from 'sonner'
 
-import { Order } from '@/api/get-orders'
+import { cancelOrder } from '@/api/cancel-order'
+import { GetOrdersResponse, Order } from '@/api/get-orders'
 import { OrderStatus } from '@/components/order-status'
 import { Button } from '@/components/ui/button'
 import { Dialog, DialogTrigger } from '@/components/ui/dialog'
@@ -16,7 +19,46 @@ type Props = {
 }
 
 export function OrderTableRow({ order }: Props) {
+  const queryClient = useQueryClient()
   const [open, setOpen] = useState(false)
+
+  const canCancelOrder = ['pending', 'processing'].includes(order.status)
+
+  const { mutateAsync, isPending: isCanceling } = useMutation({
+    mutationFn: cancelOrder,
+    onSuccess: (_, { orderId }) => {
+      queryClient.setQueriesData<GetOrdersResponse>(
+        { queryKey: ['orders'] },
+        (cached) => {
+          if (cached) {
+            return {
+              ...cached,
+              orders: cached.orders.map((orderCached) => {
+                if (orderCached.orderId === orderId) {
+                  return {
+                    ...orderCached,
+                    status: 'canceled',
+                  }
+                } else {
+                  return orderCached
+                }
+              }),
+            }
+          }
+        },
+      )
+    },
+  })
+
+  async function handleCancelOrder() {
+    try {
+      await mutateAsync({ orderId: order.orderId })
+      toast.success('Pedido cancelado com sucesso')
+    } catch (error) {
+      toast.error('Erro ao cancelar pedido, tente novamente!')
+    }
+  }
+
   return (
     <TableRow>
       <TableCell>
@@ -57,9 +99,15 @@ export function OrderTableRow({ order }: Props) {
         </Button>
       </TableCell>
       <TableCell>
-        <Button variant="ghost" size="xs">
+        <Button
+          variant="ghost"
+          size="xs"
+          disabled={!canCancelOrder || isCanceling}
+          onClick={handleCancelOrder}
+        >
           <X className="mr-2 size-3" />
           Cancelar
+          {isCanceling && <Loader2 className="ml-2 size-4 animate-spin" />}
         </Button>
       </TableCell>
     </TableRow>
